96f1e07f0261944531e486f1d1dc99063623f147
Anastasia <anastasiamaschenko103@gmail.com>
2018-01-04 03:04:55 +0300

    fix new members issues

--- pes-bot.rb
+++ pes-bot.rb
@@ -5,27 +5,27 @@ require_relative 'phrases'
 Telegram::Bot::Client.run(Token.get) do |bot|
   bot.listen do |message|
     case
-    when message.text.include?('/start')
+    when message.text && message.text.include?('/start')
       bot.api.sendMessage(
         chat_id: message.chat.id,
         text: Phrases.getGreetings.sample
       )
-    when message.text.include?('/pidor')
+    when message.text && message.text.include?('/pidor')
       bot.api.sendMessage(
         chat_id: message.chat.id,
         text: Phrases.getPidor.sample
       )
-    when message.text.include?('Пэс')
+    when message.text && message.text.include?('Пэс')
       bot.api.sendMessage(
         chat_id: message.chat.id,
         text: Phrases.getRandom.sample
       )
-    when message.text.include?('joined the group via invite link')
+    when message.new_chat_members.any?
       bot.api.sendMessage(
         chat_id: message.chat.id,
         text: Phrases.getNewMembers.sample
       )
-    when message.text.include?('left the group')
+    when message.left_chat_member
       bot.api.sendMessage(
         chat_id: message.chat.id,
         text: Phrases.getLeftMembers.sample
